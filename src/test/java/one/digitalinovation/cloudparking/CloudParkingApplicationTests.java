a0deb00470045ae3e433bb6abacbb1c70d5f6d0b
Carlos Eduardo <72090014+carlossfb@users.noreply.github.com>
2022-08-05 23:01:21 -0300

    commit inicial

---
+++ src/test/java/one/digitalinovation/cloudparking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.cloudparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
