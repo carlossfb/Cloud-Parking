7bc9037ead6f954add2b8d03045dad9905aeee20
Carlos Eduardo <72090014+carlossfb@users.noreply.github.com>
2022-08-05 23:39:33 -0300

    Teste no heroku sem data jpa e spring security

--- src/main/java/one/digitalinovation/cloudparking/controller/HelloController.java
+++ src/main/java/one/digitalinovation/cloudparking/controller/HelloController.java
@@ -1,12 +1,16 @@
 package one.digitalinovation.cloudparking.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
-    @GetMapping("/")
+
+    @GetMapping
     public String Hello(){
         return "Works";
     }
+
 }
