8a13c16fc0b4bca48c5db7afb9b4f564f4fc78f1
Carlos Eduardo <72090014+carlossfb@users.noreply.github.com>
2022-08-05 23:31:25 -0300

    Teste no heroku

---
+++ src/main/java/one/digitalinovation/cloudparking/controller/HelloController.java
@@ -0,0 +1,12 @@
+package one.digitalinovation.cloudparking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String Hello(){
+        return "Works";
+    }
+}
