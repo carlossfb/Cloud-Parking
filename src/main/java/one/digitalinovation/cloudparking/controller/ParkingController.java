313da930ff3a4818dc4b62527aa4de4da0ef5ce7
carlossfb <carloseduardofreireb@gmail.com>
2022-08-07 22:27:19 -0300

    Alterando metadados da documentação da API com Swagger

--- src/main/java/one/digitalinovation/cloudparking/controller/ParkingController.java
+++ src/main/java/one/digitalinovation/cloudparking/controller/ParkingController.java
@@ -1,5 +1,7 @@
 package one.digitalinovation.cloudparking.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import one.digitalinovation.cloudparking.entity.Parking;
 import one.digitalinovation.cloudparking.controller.dto.ParkingDTO;
 import one.digitalinovation.cloudparking.service.ParkingService;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "Parking Controller")
 public class ParkingController {
 
     private final ParkingService service;
@@ -22,6 +25,8 @@ public class ParkingController {
         this.service = service;
         this.mapper = mapper;
     }
+
+    @ApiOperation("Find all Parkings")
     @GetMapping("all")
     public ResponseEntity<List<ParkingDTO>> getAll(){
        List<ParkingDTO> response = service
@@ -31,6 +36,8 @@ public class ParkingController {
 
        return ResponseEntity.ok(response);
     }
+
+    @ApiOperation("Create a new Parking")
     @PostMapping("/new")
     public ResponseEntity<ParkingDTO> createParking(@RequestBody Parking parking){
         parking = service.create(parking);
@@ -38,6 +45,8 @@ public class ParkingController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @ApiOperation("Find Parking by ID")
     @GetMapping("/{id}")
     public ResponseEntity<ParkingDTO> getId(@PathVariable("id") String id){
         Parking parking = service.findById(id);
@@ -45,12 +54,17 @@ public class ParkingController {
 
         return ResponseEntity.ok(response);
     }
+
+    @ApiOperation("Delete Parking by ID")
     @DeleteMapping("/delete/{id}")
     public void deleteId(@PathVariable("id") String id){
         service.delete(id);
     }
+
+    @ApiOperation("Update Parking by ID")
     @PutMapping("/update/{id}")
     public ResponseEntity<ParkingDTO> updateId(@PathVariable("id") String id, @RequestBody Parking edit){
+        service.findById(id);
         Parking parking = service.updateParking(id, edit);
         ParkingDTO response = toParkingDTO(parking);
 
